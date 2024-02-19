81205d36f0a22c06cff1fb3dc35c64b73391f097
Zakaria-24 <zakariajoarder.bd@gmail.com>
2024-02-19 22:38:00 +0600

    allmost complete

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,30 @@
 let count = 0;
 let currentSeats = 40;
-const price = 500;
+const price = 550;
+
 const seats = document.querySelectorAll(".A");
-
-// for (let index = 0; index < seats.length; index++) {
-//     const A = seats[index];
-//     A.addEventListener("click", function() {
-//         console.log("boss");
-//     });
-
-// }
 for (const A of seats) {
     A.addEventListener("click", function (e) {
         count += 1;
         setInnerText("count-seat", count);
 
-        if (count > 4) {
-            alert("You can't select more than four");
-            document.getElementById("count-seat").innerText = 0;
-        }
 
-
-        // setInnerText("seats-left",currentSeats);
         const currentSeat = getTextElementValueById("seats-left");
         const updateSeatNumber = currentSeat - 1;
         setInnerText("seats-left", updateSeatNumber);
         if (updateSeatNumber === 0) {
             alert("Seat Unavailable");
             document.getElementById("seats-left").innerText = 0;
+
+        } else if (count > 4) {
+
+            alert("You can't select more than four");
+            document.getElementById("count-seat").innerText = 0;
+
+            element.disabled = false;
+
         }
+
 
 
         const seatNumber = event.target.innerText;
@@ -43,7 +39,7 @@ for (const A of seats) {
         p1.innerText = "Economy";
 
         const p2 = document.createElement("p");
-        p2.innerText = 500;
+        p2.innerText = 550;
 
         li.appendChild(p);
         li.appendChild(p1);
@@ -68,8 +64,8 @@ applyBtn.addEventListener("click", function () {
 
     const coupon = document.getElementById("coupon-apply-input").value;
     const newCoupon = coupon.split(" ").join("").toUpperCase();
-   
-   
+
+
     if (newCoupon === "NEW15") {
         const grandTotal = parseInt(getTextElementValueById('grand-total'));
 
@@ -78,6 +74,9 @@ applyBtn.addEventListener("click", function () {
 
         setInnerText("grand-total", newDiscountPrice);
         document.getElementById("coupon-apply-input").value = "";
+
+        event.target.parentNode.style.display = 'none';
+
     } else if (coupon === "Couple 20") {
         const grandTotal = parseInt(getTextElementValueById('grand-total'));
         const coupleDiscountAmount = price * 0.20;
@@ -85,11 +84,34 @@ applyBtn.addEventListener("click", function () {
 
         setInnerText("grand-total", coupleDiscountPrice);
         document.getElementById("coupon-apply-input").value = "";
-        
-    }else {
-        alert("Coupon code is not valid");
+
+        event.target.parentNode.style.display = 'none';
+
+    } else {
+        alert("Coupon code is not valid! So, you will not get any discount..!");
         document.getElementById("coupon-apply-input").value = "";
     }
+
+
+    const discountDiv = document.getElementById("discount");
+    const li = document.createElement("li");
+
+    const p = document.createElement("p");
+    p.innerText = "Discount";
+
+    const p2 = document.createElement("p")
+    if (newCoupon === "NEW15") {
+        p2.innerText = price * 0.15;
+    } else if (coupon === "Couple 20") {
+        p2.innerText = price * 0.2;
+    }else{
+        p2.innerText = 0;
+    }
+
+
+    li.appendChild(p);
+    li.appendChild(p2)
+    discountDiv.appendChild(li);
 
 })
 
